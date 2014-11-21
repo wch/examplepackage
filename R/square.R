5c58a0a9e402502874cf45cb750790ef1e902cee
Winston Chang <winston@stdout.org>
2014-11-20 20:24:53 -0600

    Added some documentation and updated functions

--- R/square.R
+++ R/square.R
@@ -1,3 +1,15 @@
+#' @export
 square <- function(x) {
-  x^2
+  powr(x, 2)
+}
+
+# This function is not exported
+powr <- function(x, y) {
+  # This is a terrible, super slow algorithm
+  val <- 1
+  while (y > 1) {
+    val <- val*x
+    y <- y-1
+  }
+  val
 }
